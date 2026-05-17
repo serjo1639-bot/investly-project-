4955585fb9871ffadc060f966435b3be135d5f3e
Mohamed Sowan <msowan89@gmail.com>
2026-05-17 12:42:03 +0200

    Remove duplicated Backend folder

--- Investly_Backend/Backend/Interfaces/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-namespace InvestlyFullAPI.Interfaces;
-
-// Handles sending emails (notifications, confirmations, etc.)
-public interface IEmailService
-{
-    // Send a simple email to one recipient
-    Task SendEmailAsync(string to, string subject, string body);
-}
